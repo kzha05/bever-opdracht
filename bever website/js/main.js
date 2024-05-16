137c2192dd00dda9c289935402f614f5f3f81334
Roy-Aa <34898@ma-web.nl>
2024-05-16 12:56:58 +0200

    bever animatie en scroll functie

---
+++ bever website/js/main.js
@@ -0,0 +1,19 @@
+function dropImage() {
+    var image = document.getElementById("beaver");
+    image.style.display = "block"; 
+
+    var yPos = 0; 
+    var dropInterval = setInterval(function() {
+      yPos += 1.5; 
+      image.style.top = yPos + "rem";
+      image.style.rotate = "-30deg"
+      
+     
+      if (yPos >= window.innerHeight) {
+        clearInterval(dropInterval);
+        image.style.display = "none"; 
+        image.style.top = "px"; 
+        ;
+      }
+    }, 5); 
+  }
