4dd048ce61a2dd6dffc16c88f942b5804b3d332f
Roy-Aa <34898@ma-web.nl>
2024-06-03 12:44:24 +0200

    bever fix

--- bever website/js/main.js
+++ bever website/js/main.js
@@ -1,4 +1,11 @@
 function dropImage() {
   const beaver = document.getElementById('beaver');
   beaver.classList.add('drop');
+
+  beaver.addEventListener('animationend', () => {
+    beaver.classList.add('hidden');
+    setTimeout(() => {
+      beaver.classList.remove('drop', 'hidden');
+    }, 1000); // Wacht 1 seconde voordat de bever weer verschijnt
+  });
 }
