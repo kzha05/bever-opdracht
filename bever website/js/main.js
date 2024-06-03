a75835ffc3c5e9f0af2cafcf5a2170f2a3d1efa8
Roy-Aa <34898@ma-web.nl>
2024-06-03 12:19:02 +0200

    bever animatie fix, model js files toegevoegd en images naar webp

--- bever website/js/main.js
+++ bever website/js/main.js
@@ -1,23 +1,4 @@
-// Bever drop function
-
 function dropImage() {
-    var image = document.getElementById("beaver");
-    image.style.display = "block"; 
-
-    var yPos = 0; 
-    var dropInterval = setInterval(function() {
-      yPos += 1.5; 
-      image.style.top = yPos + "rem";
-      image.style.rotate = "-30deg"
-      
-     
-      if (yPos >= window.innerHeight) {
-        clearInterval(dropInterval);
-        image.style.display = "none"; 
-        image.style.top = "px"; 
-        ;
-      }
-    }, 5); 
-  }
-
-// Bever drop function
+  const beaver = document.getElementById('beaver');
+  beaver.classList.add('drop');
+}
