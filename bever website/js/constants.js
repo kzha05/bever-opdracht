124bba33af35746bb9c43914ff8f0febb470e83a
kzha05 <diya.jawalapersad@gmail.com>
2024-06-17 11:33:46 +0200

    fixed slider

--- bever website/js/constants.js
+++ bever website/js/constants.js
@@ -3,4 +3,5 @@ const body = document.querySelector('body');
 const animationContainer = document.getElementById('animation-container');
 const animationHitbox = document.getElementById('fire-animation-button');
 
+
 export { beaver, body, animationContainer, animationHitbox }
