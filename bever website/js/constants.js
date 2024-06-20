e80e84f4e770fcb7af3c692225da7d304d14c03c
Dion Ahiayibor <Dionzone@hotmail.com>
2024-06-20 10:44:48 +0200

    bever gif laden

--- bever website/js/constants.js
+++ bever website/js/constants.js
@@ -2,5 +2,6 @@ const beaver = document.getElementById('beaver');
 const body = document.querySelector('body');
 const animationContainer = document.getElementById('animation-container');
 const animationHitbox = document.getElementById('fire-animation-button');
+const beaverGif = document.getElementById('beaver-walking')
 
-export { beaver, body, animationContainer, animationHitbox }
+export { beaver, body, animationContainer, animationHitbox, beaverGif }
